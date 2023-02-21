d480bd9acc95d3134e20addd35871a1309ae5de3
klaus <kgf@omniversum.de>
2023-02-21 16:44:00 +0100

    Added AAT dict

--- data_raw/dict_generator.R
+++ data_raw/dict_generator.R
@@ -1,8 +1,14 @@
+library(tidyverse)
 GAR_dict_raw <- read.csv(fileEncoding = "UTF8",
                      file = "./data_raw/GAR_dict.csv",
                      stringsAsFactors = FALSE,
-                     header = TRUE)
+                     header = TRUE) %>% as_tibble()
 
-GAR_dict <- psychTestR::i18n_dict$new(GAR_dict_raw)
+GAR_dict_raw_AAT <- read.csv(fileEncoding = "UTF8",
+                            file = "./data_raw/GAR_dict_AAT.csv",
+                            stringsAsFactors = FALSE,
+                            header = TRUE) %>% as_tibble()
+
+GAR_dict <- psychTestR::i18n_dict$new(bind_rows(GAR_dict_raw, GAR_dict_raw_AAT) %>% distinct())
 
 usethis::use_data(GAR_dict, overwrite = TRUE)
