4bdca69fe2dd5a6716d0643b6bfe765dee6e8a39
klaus <kgf@omniversum.de>
2023-10-09 20:44:28 +0200

    Further works on item evaluation.

--- R/aat_item_evaluation.R
+++ R/aat_item_evaluation.R
@@ -12,12 +12,41 @@ aat_item_evaluation <- function(label = "AAT_EVAL",
                                 nested = F,
                                 dict = GAR::GAR_dict,
                                 max_items = 43,
+                                with_intro = T,
+                                sep = " ... ",
                                 ...) {
   items <- GAR_dict %>%
     as.data.frame() %>%
     filter(stringr::str_detect(key, "TGAR_AAT_M")) %>%
     pull(key)
+
   items <- items[1:max(min(max_items, 43), 1)]
+  style <- "text-align:left;width:60%;margin-left:20%;margin-right:20%"
+
+  overall_intro_page <- psychTestR::new_timeline(
+    psychTestR::one_button_page(
+    body = shiny::p(
+      psychTestR::i18n("TGAR_ITEM_EVAL_INTRO", sub = list(num_pairs = max_items)),
+      style = style),
+    button_text = psychTestR::i18n("CONTINUE")),
+    dict = GAR::GAR_dict)
+
+  opposite_intro_page <- psychTestR::new_timeline(
+    psychTestR::one_button_page(
+    body = shiny::p(
+      psychTestR::i18n("TGAR_ITEM_EV1_INTRO"),
+      style = style),
+    button_text = psychTestR::i18n("CONTINUE")),
+    dict = GAR::GAR_dict)
+
+  suitability_intro_page <- psychTestR::new_timeline(
+    psychTestR::one_button_page(
+    body = shiny::p(
+      psychTestR::i18n("TGAR_ITEM_EV2_INTRO"),
+      style = style),
+    button_text = psychTestR::i18n("CONTINUE")),
+    dict = GAR::GAR_dict)
+
   if(nested){
     pages <- psychTestR::join(
       lapply(items, function(ik)
@@ -34,29 +63,32 @@ aat_item_evaluation <- function(label = "AAT_EVAL",
       psychTestR::new_timeline(
         make_aat_item_eval_pages(ik, scale_length =  scale_length, pages = 2),
         dict = dict))
-    pages <- psychTestR::join(pages1, pages2)
+    pages <- psychTestR::join(if(with_intro) opposite_intro_page,
+                              pages1,
+                              if(with_intro) suitability_intro_page,
+                              pages2)
 
   }
   psychTestR::join(
     psychTestR::begin_module(label = label),
+    if(with_intro) overall_intro_page,
     pages,
     psychTestR::end_module()
   )
 
 }
 
-make_aat_item_eval_pages <- function(item_key, dict = GAR::GAR_dict, scale_length = 5, pages = 1:2){
+make_aat_item_eval_pages <- function(item_key, dict = GAR::GAR_dict, scale_length = 5, pages = 1:2, sep = " ... "){
   #browser()
-  sep <- "..."
   page_label <- sprintf(stringr::str_remove(item_key, "TGAR_AAT_") %>% str_remove("_PROMPT"))
   label_keys <- sprintf("TGAR_AAT_L%s_CHOICE%%01d", scale_length)
   choices <- as.character(1:scale_length)
   button_style <- sprintf("min-width:%dpx", ifelse(scale_length == 7, 250, 0))
   page1 <-  psychTestR::NAFC_page(label = sprintf("%s_1", page_label),
                                   prompt = shiny::p(
-                                    shiny::h4(psychTestR::i18n("TGAR_AAT_EV1_PREAMBLE_SINGLE")),
-                                    shiny::h4(shiny::tags$b(
-                                      stringr::str_replace(psychTestR::i18n(item_key), "_", sep)))),
+                                    shiny::h3(shiny::tags$b(
+                                      stringr::str_replace(psychTestR::i18n(item_key), "_", sep))),
+                                  shiny::h4(psychTestR::i18n("TGAR_AAT_EV1_PREAMBLE_SINGLE"))),
                                   choices = choices,
                                   labels = sapply(1:scale_length, function(x) psychTestR::i18n(sprintf(label_keys, x)), simplify = T, USE.NAMES = T),
                                   save_answer = T,
@@ -64,9 +96,9 @@ make_aat_item_eval_pages <- function(item_key, dict = GAR::GAR_dict, scale_lengt
                                   arrange_vertically = scale_length > 5)
   page2 <-  psychTestR::NAFC_page(label = sprintf("%s_2", page_label),
                                   prompt = shiny::p(
-                                    shiny::h4(psychTestR::i18n("TGAR_AAT_EV2_PREAMBLE_SINGLE")),
-                                    shiny::h4(shiny::tags$b(
-                                      stringr::str_replace(psychTestR::i18n(item_key), "_", sep)))),
+                                    shiny::h3(shiny::tags$b(
+                                      stringr::str_replace(psychTestR::i18n(item_key), "_", sep))),
+                                  shiny::h4(psychTestR::i18n("TGAR_AAT_EV2_PREAMBLE_SINGLE"))),
                                   choices = choices,
                                   labels = sapply(1:scale_length, function(x) psychTestR::i18n(sprintf(label_keys, x)), simplify = T, USE.NAMES = T),
                                   save_answer = T,
